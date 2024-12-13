ab1aa6a3425fa83ac432965a60f0d4db4fae7fe1
Sayam Alvi <sayamalvi07@gmail.com>
2024-12-13 18:27:01 +0530

    initialized db

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,13 @@
 import app from './app';
 import logger from './config/logger';
 import config from 'config';
+import { initDB } from './config/db';
 
-const startServer = () => {
+const startServer = async () => {
     const PORT: number = config.get('server.port') ?? 5502;
     try {
+        await initDB();
+        logger.info('Database connected');
         app.listen(PORT, () => logger.info(`Listening on port ${PORT}`));
     } catch (err: unknown) {
         if (err instanceof Error) {
@@ -16,4 +19,4 @@ const startServer = () => {
     }
 };
 
-startServer();
+void startServer();
