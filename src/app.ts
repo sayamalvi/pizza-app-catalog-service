30366c4e006a0d61370d0408d335c1a3054e2980
Sayam Alvi <sayamalvi07@gmail.com>
2024-12-18 06:31:04 +0530

    add cors

--- src/app.ts
+++ src/app.ts
@@ -4,8 +4,19 @@ import { globalErrorHandler } from './common/middlewares/globalErrorHandler';
 import categoryRouter from './category/category-router';
 import cookieParser from 'cookie-parser';
 import productRouter from './product/product-router';
+import cors from 'cors';
 
 const app = express();
+const ALLOWED_DOMAINS = [
+    config.get('frontend.clientUI'),
+    config.get('frontend.adminUI'),
+];
+app.use(
+    cors({
+        origin: ALLOWED_DOMAINS as string[],
+        credentials: true,
+    }),
+);
 
 app.get('/', (req: Request, res: Response) => {
     res.send({ message: config.get('server.port') });
@@ -15,7 +26,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use('/categories', categoryRouter);
-app.use('/product', productRouter);
+app.use('/products', productRouter);
 
 app.use(globalErrorHandler);
 
